24c8f1033a3489d58e3554703bc3f32639d6fa02
Tristan Curtis <contact@tristancurtis.com>
2020-12-22 10:19:05 -0500

    Update UserInput.vue

--- src/UserInput.vue
+++ src/UserInput.vue
@@ -8,10 +8,7 @@
       <span class='delete-file-message' @click="cancelFile(index)"><img :src="icons.closeSvg.img" :alt="icons.closeSvg.name" height="10px" style="height: 10px" title='Remove the file' /></span>
     </div>
     </div>
-    <template v-slot:message-input="scopedProps">
-      <slot name="message-input">
-      </slot>
-    </template>
+    <slot name="message-input"></slot>
     <form class="sc-user-input" :class="{active: inputActive}" :style="{background: colors.userInput.bg}">
       <div
         role="button"
